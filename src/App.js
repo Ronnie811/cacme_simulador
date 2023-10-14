888efd01348fa9917f5f7364c45599bdcf6ba1be
Christian Barahona <christian.barahona@globant.com>
2023-10-14 09:48:25 -0500

    Agregar nuevos tipos de crédito

--- src/App.js
+++ src/App.js
@@ -1,7 +1,35 @@
-import React, {useEffect, useState } from "react";
+import React, {useEffect, useState, useRef } from "react";
+
+const creditConfigs = [
+  {
+    "tipoCredito": "Consumo",
+    "interes": 15.5,
+    "montoMinimo": 1,
+    "montoMaximo": 1000000
+  },
+  {
+    "tipoCredito": "Microcrédito minorista",
+    "interes": 24.0,
+    "montoMinimo": 100,
+    "montoMaximo": 1000
+  },
+  {
+    "tipoCredito": "Micrecrédito acumulación simple",
+    "interes": 22.0,
+    "montoMinimo": 1001,
+    "montoMaximo": 10000
+  },
+  {
+    "tipoCredito": "Micrecrédito acumulación ampliada",
+    "interes": 20.0,
+    "montoMinimo": 10001,
+    "montoMaximo": 100000
+  }
+];
 
 const App = () => {
-  const [creditType, setCreditType] = useState("Consumo");
+  const [creditType, setCreditType] = useState(creditConfigs[0].tipoCredito);
+  const [currentCreditConfig, setCurrentCreditConfig] = useState(creditConfigs[0]);
   const [amount, setAmount] = useState(0);
   const [loanTerm, setLoanTerm] = useState(0);
   const [paymentType, setPaymentType] = useState("Cuota fija");
@@ -10,37 +38,53 @@ const App = () => {
   const [totalCapital, setTotalCapital] = useState(0);
   const [totalInterest, setTotalInterest] = useState(0);
   const [totalPayment, setTotalPayment] = useState(0);
-  const [nominalRate, setNominalRate] = useState(0);
   const [clickCount, setClickCount] = useState(0);
   const [pnlAmortizacionStyle, setPnlAmortizacionStyle] = useState("hidden");
+  const [errorMessage, setErrorMessage] = useState("");
+  const errorTimeout = useRef(null);
   
 
   const handleQuote = () => {
+    const errors = [];
+    if (amount < currentCreditConfig.montoMinimo || amount > currentCreditConfig.montoMaximo) {
+      errors.push(`El monto debe estar entre ${currentCreditConfig.montoMinimo} y ${currentCreditConfig.montoMaximo}`);
+    }
+    if (loanTerm <= 0) {
+      errors.push("El plazo de pago en meses debe ser mayor que cero.");
+    }
+    if (loanTerm > 1200) {
+      errors.push("El plazo de pago en meses no puede ser mayor a 1200.");
+    }
+
+    if(errors.length > 0){
+      setErrorMessage(errors);
+      if (errorTimeout.current) clearTimeout(errorTimeout.current);
+      errorTimeout.current = setTimeout(() => {
+        setErrorMessage([]);
+      }, 10000);
+      return;
+    }
+
     setClickCount(clickCount+1);
     document.getElementById("pnl-amortizacion").classList.remove("hidden");
   };
 
-  useEffect(() => {
-    let nominalRate;
-    switch (creditType) {
-      case "Consumo":
-        nominalRate = 10;
-        break;
-      case "Vivienda":
-        nominalRate = 10;
-        break;
-      case "Microcrédito":
-        nominalRate = 21;
-        break;
-      case "Comercial":
-        nominalRate = 10;
-        break;
-      default:
-        return;
-    }
-    setNominalRate(nominalRate)
+  const handleCloseError = () => {
+    setErrorMessage("");
+    if (errorTimeout.current) clearTimeout(errorTimeout.current);
+  }
 
-    const rate = (1 + (nominalRate / 100) / 12) ** 12 - 1;
+  const handleChangeCreditType = (e) => {
+    const creditConfig = creditConfigs[e.target.value];
+    setCreditType(creditConfig.tipoCredito)
+    
+    setCurrentCreditConfig(creditConfig);
+    console.log("min, max",creditConfig.montoMinimo,creditConfig.montoMaximo);
+  }
+
+  useEffect(() => {    
+    
+    const rate = (1 + (currentCreditConfig.interes / 100) / 12) ** 12 - 1;
     setEffectiveRate(rate * 100);
 
     let table = [];
@@ -98,84 +142,87 @@ const App = () => {
   
 
   return (
-    <div className="container mx-auto p-4">
+    <div className="container mx-auto p-4 relative">
+      {errorMessage.length > 0 && (
+      <div className="absolute top-0 right-0 bg-red-500 text-white p-4 rounded-lg shadow-lg cursor-pointer z-10" onClick={handleCloseError}>
+        <span className="absolute top-2 right-2 font-bold cursor-pointer" title="Cerrar">✕</span>
+        <ul className="mt-2 mr-2">
+          {errorMessage.map((error, index) => (
+            <li key={index}>{error}</li>
+          ))}
+        </ul>
+      </div>
+    )}
       <h1 className="text-3xl font-bold mb-4">Cotizador de crédito</h1>
       
-      <form>
-        <div className="mb-4">
-          <label>Tipo de crédito: </label>
-          <select className="border" onChange={e => {setCreditType(e.target.value);document.getElementById("pnl-amortizacion").classList.add("hidden")}}>
-            <option>Consumo</option>
-            <option>Vivienda</option>
-            <option>Microcrédito</option>
-            <option>Comercial</option>
+      <form className="flex flex-col w-1/3 mb-10">
+        <div className="mb-5 flex flex-col">
+          <label>Tipo de crédito </label>
+          <select className="border" onChange={e => { handleChangeCreditType(e); document.getElementById("pnl-amortizacion").classList.add("hidden") }}>
+            {creditConfigs.map((config, index) => (
+              <option key={index} value={index}>{config.tipoCredito}</option>
+            ))}
           </select>
         </div>
 
-        <div className="mb-4 bg-red-500">
-          {/* Ícono de advertencia y mensaje aquí */}
+        <div className="mb-5 bg-sky-200 flex rounded-lg">
+          <div className="ml-5">ℹ️</div>
+          <div className="ml-3"> Monto mínimo: {currentCreditConfig.montoMinimo}, Monto máximo: {currentCreditConfig.montoMaximo}</div>
         </div>
         
-        <div className="mb-4">
-          <label>Monto: </label>
-          <input type="number" className="border" onChange={e => setAmount(Number(e.target.value))} />
-          {/* Ícono de información aquí */}
+        <div className="mb-5 flex flex-col">
+          <label>Monto </label>
+          <input type="number" className="border" onChange={e => {setAmount(Number(e.target.value));document.getElementById("pnl-amortizacion").classList.add("hidden");}} />
         </div>
 
-        <div className="mb-4">
-          <label>Plazo de pago: </label>
-          <input type="number" className="border" onChange={e => setLoanTerm(Number(e.target.value))} />
+        <div className="mb-5 flex flex-col">
+          <label>Plazo de pago en meses</label>
+          <input type="number" className="border" onChange={e => {setLoanTerm(Number(e.target.value));document.getElementById("pnl-amortizacion").classList.add("hidden");}} />
         </div>
 
-        <div className="mb-4">
-          <input type="radio" id="fixed" name="paymentType" value="Cuota fija" onChange={e => setPaymentType(e.target.value)} />
+        <div className="mb-5">
+          <input type="radio" id="fixed" name="paymentType" value="Cuota fija" checked={paymentType === "Cuota fija"} onChange={e => setPaymentType(e.target.value)} />
           <label htmlFor="fixed">&nbsp;Cuota fija&nbsp;&nbsp;&nbsp;&nbsp;</label>
           
-          <input type="radio" id="variable" name="paymentType" value="Capital fijo" onChange={e => setPaymentType(e.target.value)} />
+          <input type="radio" id="variable" name="paymentType" value="Capital fijo" checked={paymentType === "Capital fijo"} onChange={e => setPaymentType(e.target.value)} />
           <label htmlFor="variable">&nbsp;Capital fijo</label>
         </div>
 
-        <button type="button"  className="border p-2 rounded-full" onClick={handleQuote}>
+        <button type="button"  className="border p-2 rounded-full bg-indigo-100" onClick={handleQuote}>
           Cotizar
         </button>
       </form>
-
+      <div className="mt-5">
       <div id="pnl-amortizacion" className={pnlAmortizacionStyle}>
-        <h2>Tasa Nominal: {nominalRate}</h2>
+        <h2>Tasa Nominal: {currentCreditConfig.interes}</h2>
         <h2>Tasa Efectiva: {effectiveRate.toFixed(2)}%</h2>
-        {/* ...Otras informaciones como fechas... */}
-        <table className="border p-5">
-          <thead>
-            <tr>
-              <th>Nro</th>
-              <th>Capital</th>
-              <th>Interés</th>
-              <th>Cuota</th>
-              <th>Saldo</th>
-            </tr>
-          </thead>
-          <tbody>
-            {amortizationTable.map((row, index) => (
-              <tr key={index}>
-                <td>{row.Nro}</td>
-                <td>{row.Capital.toFixed(2)}</td>
-                <td>{row.Interes.toFixed(2)}</td>
-                <td>{row.Cuota.toFixed(2)}</td>
-                <td>{Math.abs(row.Saldo.toFixed(2))}</td>
-              </tr>
-            ))}
-          </tbody>
-          <tfoot>
-          <tr>
-            <td>Total:</td>
-            <td>{loanTerm}</td>
-            <td>{totalCapital.toFixed(2)}</td>
-            <td>{totalInterest.toFixed(2)}</td>
-            <td>{totalPayment.toFixed(2)}</td>
-            
-          </tr>
-        </tfoot>
-        </table>
+        <div className="grid grid-cols-5 border mt-5">
+          {/* Encabezado */}
+          <div className="col-span-1 font-bold p-2">Nro</div>
+          <div className="col-span-1 font-bold p-2">Capital</div>
+          <div className="col-span-1 font-bold p-2">Interés</div>
+          <div className="col-span-1 font-bold p-2">Cuota</div>
+          <div className="col-span-1 font-bold p-2">Saldo</div>
+
+          {/* Cuerpo */}
+          {amortizationTable.map((row, index) => (
+            <React.Fragment key={index}>
+              <div className={`col-span-1 p-2 ${index % 2 === 0 ? 'bg-gray-100' : 'bg-white'}`}>{row.Nro}</div>
+              <div className={`col-span-1 p-2 ${index % 2 === 0 ? 'bg-gray-100' : 'bg-white'}`}>{row.Capital.toFixed(2)}</div>
+              <div className={`col-span-1 p-2 ${index % 2 === 0 ? 'bg-gray-100' : 'bg-white'}`}>{row.Interes.toFixed(2)}</div>
+              <div className={`col-span-1 p-2 ${index % 2 === 0 ? 'bg-gray-100' : 'bg-white'}`}>{row.Cuota.toFixed(2)}</div>
+              <div className={`col-span-1 p-2 ${index % 2 === 0 ? 'bg-gray-100' : 'bg-white'}`}>{Math.abs(row.Saldo.toFixed(2))}</div>
+            </React.Fragment>
+          ))}
+
+          {/* Pie */}
+          <div className="col-span-1 font-bold p-2 bg-gray-300">Total:</div>
+          <div className="col-span-1 font-bold p-2 bg-gray-300">{loanTerm}</div>
+          <div className="col-span-1 font-bold p-2 bg-gray-300">{totalCapital.toFixed(2)}</div>
+          <div className="col-span-1 font-bold p-2 bg-gray-300">{totalInterest.toFixed(2)}</div>
+          <div className="col-span-1 font-bold p-2 bg-gray-300">{totalPayment.toFixed(2)}</div>
+        </div>
+      </div>
       </div>
     </div>
   );
